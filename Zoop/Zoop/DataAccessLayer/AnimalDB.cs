67c713a4c242ab2c28565667c1931facdebd265d
Haartmans,Dylan D.J.M.I <d.haartmans@student.fontys.nl>
2024-03-26 19:38:40 +0000

    Web Design Basic part 1

---
+++ Zoop/Zoop/DataAccessLayer/AnimalDB.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace DataAccessLayer
+{
+    public class AnimalDB
+    {
+        private DatabaseConnection dbManager = new DatabaseConnection();
+
+        public void AddAnimal(string name, string species, int age, string gender, decimal weight, string color, bool isSick, string location, DateTime lastFeedTime)
+        {
+            string query = "INSERT INTO Animals (Name, Species, Age, Gender, Weight, Color, IsSick, Location, LastFeedTime) VALUES (@Name, @Species, @Age, @Gender, @Weight, @Color, @IsSick, @Location, @LastFeedTime)";
+            using (SqlConnection connection = dbManager.GetConnection())
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Name", name);
+                    command.Parameters.AddWithValue("@Species", species);
+                    command.Parameters.AddWithValue("@Age", age);
+                    command.Parameters.AddWithValue("@Gender", gender);
+                    command.Parameters.AddWithValue("@Weight", weight);
+                    command.Parameters.AddWithValue("@Color", color);
+                    command.Parameters.AddWithValue("@IsSick", isSick);
+                    command.Parameters.AddWithValue("@Location", location);
+                    command.Parameters.AddWithValue("@LastFeedTime", lastFeedTime);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<IDictionary<string, object>> GetAllAnimals()
+        {
+            List<IDictionary<string, object>> animals = new List<IDictionary<string, object>>();
+            string query = "SELECT * FROM Animals";
+
+            try
+            {
+                using (SqlConnection connection = dbManager.GetConnection())
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                var animal = new Dictionary<string, object>();
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    animal[reader.GetName(i)] = reader.GetValue(i);
+                                }
+                                animals.Add(animal);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred while retrieving animals: " + ex.Message);
+            }
+
+            return animals;
+        }
+
+        public void DeleteAnimal(int animalID)
+        {
+            string query = "DELETE FROM Animals WHERE AnimalID = @AnimalID";
+
+            try
+            {
+                using (SqlConnection connection = dbManager.GetConnection())
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@AnimalID", animalID);
+
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred while deleting animal: " + ex.Message);
+                throw;
+            }
+        }
+
+        public void UpdateAnimal(int animalID, string name, string species, int age, string gender, decimal weight, string color, bool isSick, string location, DateTime lastFeedTime)
+        {
+            string query = "UPDATE Animals SET Name = @Name, Species = @Species, Age = @Age, Gender = @Gender, Weight = @Weight, Color = @Color, IsSick = @IsSick, Location = @Location, LastFeedTime = @LastFeedTime WHERE AnimalID = @AnimalID";
+
+            try
+            {
+                using (SqlConnection connection = dbManager.GetConnection())
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Name", name);
+                        command.Parameters.AddWithValue("@Species", species);
+                        command.Parameters.AddWithValue("@Age", age);
+                        command.Parameters.AddWithValue("@Gender", gender);
+                        command.Parameters.AddWithValue("@Weight", weight);
+                        command.Parameters.AddWithValue("@Color", color);
+                        command.Parameters.AddWithValue("@IsSick", isSick);
+                        command.Parameters.AddWithValue("@Location", location);
+                        command.Parameters.AddWithValue("@LastFeedTime", lastFeedTime);
+                        command.Parameters.AddWithValue("@AnimalID", animalID);
+
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred while updating animal: " + ex.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ Zoop/Zoop/Domain/Entity/Reptile.cs
@@ -0,0 +1,53 @@
+﻿using Azure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class Reptile : Animal
+    {
+        private bool isColdBlooded;
+        private string scaleType;
+        private string eggType;
+
+        public Reptile(int animalID, string name, string species, int age, string gender, decimal weight, string color, bool isSick, string location, DateTime lastFeedTime, string scaleType, string eggType, bool isColdBlooded)
+            : base(animalID, name, species, age, gender, weight, color, isSick, location, lastFeedTime)
+        {
+            this.isColdBlooded = isColdBlooded;
+            this.scaleType = scaleType;
+            this.eggType = eggType;
+        }
+
+        public override string MakeSound()
+        {
+            return "The reptile hisses.";
+        }
+
+        public override string Feed()
+        {
+            LastFeedTime = DateTime.Now;
+            return "Feeding insects to the reptile.";
+        }
+
+        public void Bask()
+        {
+            if (isColdBlooded)
+            {
+            }
+        }
+
+        public void RegulateBodyTemperature()
+        {
+            // Code to regulate body temperature
+        }
+
+        public override string ToString()
+        {
+            return $"Species: {Species}, Name: {Name}, Age: {Age}, Gender: {Gender}, Scale Type: {scaleType}, Egg Type: {eggType}, Weight: {Weight}, Color: {Color}, Is Sick: {IsSick}, Location: {Location}, Last Feed Time: {LastFeedTime}";
+        }
+    }
+}
