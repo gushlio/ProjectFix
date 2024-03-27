d754529f27b86a75ffe1c105b46d8685796504f7
Haartmans,Dylan D.J.M.I <d.haartmans@student.fontys.nl>
2024-03-27 05:06:44 +0000

    ZooMap

---
+++ Zoop/DataAccessLayer/ImagesDB.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class ImagesDB
+    {
+        private DatabaseConnection dbManager = new DatabaseConnection();
+
+        public void AddImage(byte[] imageData)
+        {
+            string query = "INSERT INTO Images (ImageData) VALUES (@ImageData)";
+            try
+            {
+                using (SqlConnection connection = dbManager.GetConnection())
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ImageData", imageData);
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred while adding image: " + ex.Message);
+                throw;
+            }
+        }
+
+        public List<byte[]> GetAllImages()
+        {
+            List<byte[]> images = new List<byte[]>();
+            string query = "SELECT ImageData FROM Images";
+
+            try
+            {
+                using (SqlConnection connection = dbManager.GetConnection())
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                byte[] imageData = (byte[])reader["ImageData"];
+                                images.Add(imageData);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred while retrieving images: " + ex.Message);
+                throw;
+            }
+
+            return images;
+        }
+
+        public void DeleteImage(int imageID)
+        {
+            string query = "DELETE FROM Images WHERE ImageID = @ImageID";
+
+            try
+            {
+                using (SqlConnection connection = dbManager.GetConnection())
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ImageID", imageID);
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred while deleting image: " + ex.Message);
+                throw;
+            }
+        }
+
+        public void UpdateImage(int imageID, byte[] newImageData)
+        {
+            string query = "UPDATE Images SET ImageData = @NewImageData WHERE ImageID = @ImageID";
+
+            try
+            {
+                using (SqlConnection connection = dbManager.GetConnection())
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@NewImageData", newImageData);
+                        command.Parameters.AddWithValue("@ImageID", imageID);
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred while updating image: " + ex.Message);
+                throw;
+            }
+        }
+        public List<string> GetAllImagesAsBase64()
+        {
+            List<string> imagesBase64 = new List<string>();
+            string query = "SELECT ImageData FROM Images";
+
+            try
+            {
+                using (SqlConnection connection = dbManager.GetConnection())
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                byte[] imageData = (byte[])reader["ImageData"];
+                                string base64String = Convert.ToBase64String(imageData);
+                                imagesBase64.Add(base64String);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred while retrieving images: " + ex.Message);
+                throw;
+            }
+
+            return imagesBase64;
+        }
+
+
+    }
+}
---
+++ Zoop/Domain/Entity/Bird.cs
@@ -0,0 +1,55 @@
+﻿using Azure;
+using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class Bird : Animal
+    {
+        private bool canFly;
+        private double wingspan;
+        private string beakType;
+
+        public Bird(int animalID, string name, string species, int age, string gender, decimal weight, string color, bool isSick, string location, DateTime lastFeedTime, bool canFly, double wingspan, string beakType)
+        : base(animalID, name, species, age, gender, weight, color, isSick, location, lastFeedTime)
+        {
+            this.canFly = canFly;
+            this.wingspan = wingspan;
+            this.beakType = beakType;
+        }
+
+        public override string MakeSound()
+        {
+            return "The bird chirps.";
+        }
+
+        public override string Feed()
+        {
+            LastFeedTime = DateTime.Now;
+            return "Feeding seeds to the bird.";
+        }
+
+        public void Fly()
+        {
+            if (canFly)
+            {
+            }
+        }
+
+        public void BuildNest()
+        {
+            // Code to build a nest
+        }
+
+        public override string ToString()
+        {
+            return $"Species: {Species}, Name: {Name}, Age: {Age}, Gender: {Gender}, Wingspan: {wingspan}, Beak Type: {beakType}, Weight: {Weight}, Color: {Color}, Is Sick: {IsSick}, Location: {Location}, Last Feed Time: {LastFeedTime}";
+        }
+    }
+}
